99649e4fb18a579244c1890bc57ef312b32b0153
Ovictorchagas <victorchagas@id.uff.br>
2026-04-06 10:00:43 -0300

    Leitura e manipulação de dados, aula 7 (tarefa2).

---
+++ Script-aula7-tarefa2.R
@@ -0,0 +1,15 @@
+# Script da aula 7 - 06/04 para praticar uso de branches no Git e GitHub(tarefa 2)
+
+# 1. Leitura e manipulação dos dados
+dados = read.csv("dados-aula-7-limpos.csv", header=TRUE, sep = ",")
+# ver estrutura
+str(dados)
+head(dados)
+
+# cálculo de IMC
+dados$imc = dados$peso / ((dados$altura/100)^2)
+
+# criando faixas etárias
+dados$faixa_etaria = ifelse(dados$idade < 30, "Jovem",
+                            ifelse(dados$idade < 60, "Adulto", "Idoso"))
+dados$faixa_etaria = factor(dados$faixa_etaria)
